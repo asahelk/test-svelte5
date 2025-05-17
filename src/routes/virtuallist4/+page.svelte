6b2a073d1a6d9f1f91b741d21602ce860b8a8ad5
Asahel Zavaleta <abraham_zm22@hotmail.com>
2025-05-17 01:50:38 -0500

    feat(virtuallist4): add virtual list component with drag-and-drop functionality
    Introduce a new virtual list component with support for drag-and-drop operations. The component includes constants, state management, and actions for handling item positioning and scrolling. The implementation ensures efficient rendering of large lists by only rendering visible items and recomputing sizes dynamically. This feature enhances user interaction by allowing seamless reordering of list items.

---
+++ src/routes/virtuallist4/+page.svelte
@@ -0,0 +1,193 @@
+<script lang="ts">
+	import { dndzone, type DndEvent } from 'svelte-dnd-action';
+	import type VirtualListType from './VirtualList4.svelte';
+	import VirtualList from './VirtualList4.svelte';
+	import { overrideItemIdKeyNameBeforeInitialisingDndZones } from 'svelte-dnd-action';
+	// import { array } from './states.svelte.ts';
+
+	overrideItemIdKeyNameBeforeInitialisingDndZones('index');
+
+	let virtualList = $state<ReturnType<typeof VirtualListType>>();
+
+	let virtualListItems = $derived(
+		virtualList?.getVirtualItems().map((e, i) => {
+			if (virtualList === undefined) return e;
+			const { offset } = virtualList.getState();
+			const { start = 0, stop } = virtualList.getVisibleRange();
+			return {
+				order: start + i,
+				...e,
+			};
+		}) ?? [],
+	);
+
+	let items = $state(Array.from({ length: 34 }, (_, i) => ({ id: crypto.randomUUID(), name: `xitem ${i}` })));
+	type DnDItem = (typeof virtualListItems)[number];
+
+	let itemSize = 50;
+	// let items = $state([...array.value])
+
+	function handleSortConsider(e: CustomEvent<DndEvent<DnDItem>>) {
+		// console.log("event", JSON.stringify(e.detail,null, 2))
+		const {
+			items: partialDndItems,
+			info: { id: elementIndex, trigger },
+		} = e.detail;
+
+		console.log(`consider event => ${trigger.toUpperCase()} - ${elementIndex}`, $state.snapshot(partialDndItems));
+		if (virtualList === undefined) return;
+
+		const { offset } = virtualList.getState();
+		const { start = 0, stop } = virtualList.getVisibleRange();
+
+		console.log({ start, stop });
+
+		const newDndItems = partialDndItems.map((e, i) => {
+			return {
+				...e,
+				order: start + i,
+				style: `left:0;width:100%;height:${itemSize}px;position:absolute;top:${(start + i) * itemSize}px;'`,
+			};
+		});
+
+		// console.log("getVirtualItems()",$state.snapshot(virtualList?.getVirtualItems()))
+		// console.log("virtualListItems",$state.snapshot(virtualListItems))
+		// console.log("partialDndItems",$state.snapshot(partialDndItems))
+		// console.log("newDndItems",$state.snapshot(newDndItems))
+
+		// virtualList.setVirtualItems([...newDndItems])
+		virtualList.setVirtualItems([...newDndItems]);
+		// partialDndItems[2].order = 9
+		console.log('\n\n');
+	}
+
+	function handleSortFinalize(e: CustomEvent<DndEvent<DnDItem>>) {
+		const {
+			items: partialDndItems,
+			info: { id: elementIndex, trigger },
+		} = e.detail;
+		console.log(`finalize event => ${trigger.toUpperCase()} - ${elementIndex}`, $state.snapshot(partialDndItems));
+
+		if (virtualList === undefined) return;
+		const { offset } = virtualList.getState();
+		const { start = 0, stop } = virtualList.getVisibleRange();
+
+		// console.log({start,stop})
+
+		const newDndItems = partialDndItems.map((e, i) => {
+			return {
+				...e,
+				order: start + i,
+				style: `left:0;width:100%;height:${itemSize}px;position:absolute;top:${(start + i) * itemSize}px;'`,
+			};
+		});
+
+		// console.log("getVirtualItems()",$state.snapshot(virtualList?.getVirtualItems()))
+		// console.log("virtualListItems",$state.snapshot(virtualListItems))
+		// console.log("partialDndItems",$state.snapshot(partialDndItems))
+		// console.log("newDndItems",$state.snapshot(newDndItems))
+
+		const oldPosition = +elementIndex;
+		const auxNewPosition = newDndItems.find((e) => e.index === oldPosition)?.order;
+		const newPosition = newDndItems[auxNewPosition - 1]?.order ?? 0;
+		// console.log("oldPosition",oldPosition)
+		// console.log("auxNewPosition",auxNewPosition)
+		// console.log("newPosition",newPosition)
+
+		const elementToMove = items.splice(oldPosition, 1)[0];
+
+		// console.log("items bef",$state.snapshot(items))
+		items = items.toSpliced(auxNewPosition, 0, elementToMove);
+		virtualList.setVirtualItems(newDndItems);
+		virtualList.recomputeSizes(start);
+
+		console.log('\n\n');
+	}
+
+	function testClick() {
+		if (virtualList === undefined) return;
+
+		const { offset } = virtualList.getState();
+		const { start, stop } = virtualList.getVisibleRange();
+		console.log({ offset, start, stop });
+	}
+
+	function onscroll() {
+		// const {start, stop} = virtualList.getVisibleRange()
+		// virtualList.recomputeSizes(start)
+		// items = [...items]
+	}
+
+	// let virtualListItems = $derived(virtualList?.getVirtualItems() ?? [])
+</script>
+
+<section class="flex flex-col">
+	{items.length}
+	<div>
+		<VirtualList
+			bind:this={virtualList}
+			use={[
+				[
+					dndzone,
+					{
+						items: virtualListItems,
+						flipDurationMs: 200,
+						// dropTargetClasses: ['!outline-teal-500', '!outline-dashed'],
+						dropTargetStyle: {
+							outline: '1px dashed rgb(13 148 136)',
+						},
+						type: 'columns',
+						dragDisabled: false,
+						autoAriaDisabled: true,
+					},
+				],
+			]}
+			width="100%"
+			height={300}
+			itemCount={items.length}
+			{itemSize}
+			overscanCount={0}
+			onconsider={handleSortConsider}
+			onfinalize={handleSortFinalize}
+			wrapperRestProps={{ onscroll }}
+			getKey={(index) => index}
+		>
+			{#snippet theItem(style: string, index: number, key: number)}
+				<div class="w-full" style="width: 100%;">
+					{items[index]?.name}
+					-{index}-{key}
+				</div>
+			{/snippet}
+
+			<!-- <div slot="item" let:index let:style {style}>
+				Letter: {items[index]}, Row: #{index}
+			</div> -->
+
+			<!-- getKey={(index) => items[index].id} -->
+
+			<!-- <div slot="item" let:index let:style {style}>
+			{index}-{style}
+		</div> -->
+		</VirtualList>
+	</div>
+	<button class="cursor-pointer bg-neutral-500 p-2" onclick={testClick}> Test </button>
+</section>
+
+<style>
+	:root {
+		background: black;
+		color: white;
+	}
+	section {
+		padding: 0.3em;
+		/* this will allow the dragged element to scroll the list */
+		/* overflow: scroll; */
+		/* height: 600px; */
+	}
+	div {
+		width: 50%;
+		padding: 0.2em;
+		border: 1px solid blue;
+		margin: 0.15em 0;
+	}
+</style>
---
+++ src/routes/virtuallist4/states.svelte.ts
@@ -0,0 +1,58 @@
+
+export const array = {
+    value: [
+        {
+            "id": "7cab45e1-ee1d-4c19-82bf-4bb9aa3849d0",
+            "name": "xitem 1"
+        },
+        {
+            "id": "e6b66515-86c8-49f3-965b-191d2b7497f2",
+            "name": "xitem 2"
+        },
+        {
+            "id": "3392d7a5-4208-41ee-a179-241e22ab76f1",
+            "name": "xitem 3"
+        },
+        {
+            "id": "8122927f-f358-4bb8-8824-e0a6f031e823",
+            "name": "xitem 4"
+        }
+        // ,
+        //    {
+        //        "id": "fb31cb34-fc5c-495c-8e01-eef9e00450ba",
+        //        "name": "xitem 5"
+        //    },
+        //    {
+        //        "id": "3f870e47-bf49-4ca2-b14d-fb230eccf3e1",
+        //        "name": "xitem 6"
+        //    },
+        //    {
+        //        "id": "eda9c5e4-f47f-4c8f-9e8c-598c1768a156",
+        //        "name": "xitem 7"
+        //    },
+        //    {
+        //        "id": "ca291336-3540-46e3-a856-15aa9c03b7fc",
+        //        "name": "xitem 8"
+        //    },
+        //    {
+        //        "id": "97fb65fa-4a92-43d9-97a2-9c397c6524ba",
+        //        "name": "xitem 9"
+        //    },
+        //    {
+        //        "id": "1f2f5d31-cb09-4109-ac65-67596aebcaa0",
+        //        "name": "xitem 10"
+        //    },
+        //    {
+        //        "id": "3b5e2db6-0cca-422b-95ec-8365c0869258",
+        //        "name": "xitem 11"
+        //    },
+        //    {
+        //        "id": "cc50381e-2a18-4ced-b81c-059539862c12",
+        //        "name": "xitem 12"
+        //    },
+        //    {
+        //        "id": "9ba9fa7d-508d-42f5-81dc-72058a11e0f8",
+        //        "name": "xitem 13"
+        //    }
+    ]
+}
