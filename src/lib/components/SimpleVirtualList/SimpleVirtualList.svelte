87c9db9c4622a6683be270950a3fb538e6c109e1
Asahel Zavaleta <abraham_zm22@hotmail.com>
2025-05-19 15:22:14 -0500

    feat(SimpleVirtualList): add vertical scrolling and dnd support
    Introduce vertical scrolling functionality and drag-and-drop (dnd) event handling to the SimpleVirtualList component. Added support for scroll direction, event bundler for passive events, and animation with Svelte's flip. Also, improved the component's structure and styling for better flexibility and maintainability.

--- src/lib/components/SimpleVirtualList/SimpleVirtualList.svelte
+++ src/lib/components/SimpleVirtualList/SimpleVirtualList.svelte
@@ -1,20 +1,63 @@
-<script lang="ts" generics="T extends { isVisible?: boolean }">
-	import type { Snippet } from "svelte";
-	import { useActions, type ActionArray } from "../../../routes/virtuallist5/useActions";
+<script module lang="ts">
+	/**
+	 * the third argument for event bundler
+	 * @see https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md
+	 */
+	const thirdEventArg = (() => {
+		let result = false;
 
-	interface Props {
+		try {
+			const arg = Object.defineProperty({}, "passive", {
+				get() {
+					// @ts-ignore
+					result = { passive: true };
+					return true;
+				},
+			});
+
+			// @ts-ignore
+			window.addEventListener("testpassive", arg, arg);
+			// @ts-ignore
+			window.remove("testpassive", arg, arg);
+		} catch (e) {
+			/* */
+		}
+
+		return result;
+	})();
+</script>
+
+<script lang="ts" generics="T extends { isVisible?: boolean }">
+	import { onDestroy, onMount, type Snippet } from "svelte";
+	import { useActions, type ActionArray } from "../../../routes/virtuallist5/useActions";
+	import type { HTMLAttributes } from "svelte/elements";
+	import { flip } from "svelte/animate";
+	import type { DndEvent } from "svelte-dnd-action";
+
+	type Props = {
 		items: T[];
 		renderItem: Snippet<[index: number]>;
 		itemSize: number;
 		width: number;
-		height?: number;
+		height: number;
 		getKey?: (index: number) => unknown;
 		use?: ActionArray | undefined;
-	}
+		scrollDirection?: "horizontal" | "vertical";
+		wrapperRestProps?: HTMLAttributes<HTMLDivElement>;
+		onconsider: (e: CustomEvent<DndEvent<T>>) => void;
+		onfinalize: (e: CustomEvent<DndEvent<T>>) => void;
+	} & HTMLAttributes<HTMLDivElement>;
 
-	let { items = $bindable(), renderItem, itemSize, width, height, getKey, use = [], ...rest }: Props = $props();
+	let { items = $bindable(), renderItem, itemSize, width, height, getKey, use = [], scrollDirection, wrapperRestProps, ...props }: Props = $props();
 
 	let scrollLeft = $state(0);
+	let scrollTop = $state(0);
+
+	let wrapper: HTMLDivElement | undefined = $state();
+
+	onMount(() => {
+		wrapper?.addEventListener("scroll", onScroll as (e: Event) => void, thirdEventArg);
+	});
 
 	function onScroll(
 		event: UIEvent & {
@@ -22,6 +65,7 @@
 		},
 	) {
 		scrollLeft = event.currentTarget.scrollLeft;
+		scrollTop = event.currentTarget.scrollTop;
 		rerender();
 	}
 
@@ -30,8 +74,12 @@
 	});
 
 	function rerender() {
-		const firstIndex = Math.floor(scrollLeft / itemSize);
-		const count = Math.ceil(width / itemSize) + 1;
+		let scrollSize = scrollDirection === "vertical" ? scrollTop : scrollLeft;
+
+		const firstIndex = Math.floor(scrollSize / itemSize);
+
+		const size = scrollDirection === "vertical" ? height : width;
+		const count = Math.ceil(size / itemSize) + 1;
 
 		for (let i = 0; i < count; i++) {
 			if (items[firstIndex - i]) items[firstIndex - i].isVisible = false;
@@ -41,34 +89,44 @@
 			const item = items[firstIndex + i];
 			if (item) item.isVisible = true;
 		}
-		items = items;
+		// items = items;
 	}
 
+	let innerStyle = $derived.by(() => {
+		if (scrollDirection === "vertical") {
+			return `grid-template-rows: repeat(${items.length},1fr); height: ${itemSize * items.length}px; min-height: 100%;`;
+		}
+		return `grid-template-columns: repeat(${items.length},1fr); width: ${itemSize * items.length}px; min-height: 100%;`;
+	});
 	rerender();
 
 	//TODO: pass length, and create a new array with the length with isVisible
 	// in Svelte svelte@5.29.0 they introduced the attachments, useful to manage use:actions
 	// https://github.com/sveltejs/svelte/pull/15000 - width this useActions file is not longer needed
+
+	onDestroy(() => {
+		wrapper?.removeEventListener("scroll", onScroll as (e: Event) => void);
+	});
 </script>
 
-<section class="container-wrapper" onscroll={onScroll} style="width: {width}px; height: 100%;">
-	<div
-		class="container-inner"
-		style="grid-template-columns: repeat({items.length},1fr); width: {itemSize * items.length}px; min-height: 100%;"
-		use:useActions={use}
-		{...rest}
-	>
+<div bind:this={wrapper} class="container-wrapper" style="width: {width}px; height: {height}px;" {...wrapperRestProps}>
+	<div class="container-inner" style={innerStyle} use:useActions={use} {...props}>
 		{#each items as item, index (getKey ? getKey(index) : index)}
-			{#if item?.isVisible}
-				<div class="item bg-teal-900" style="grid-column: {index + 1};">
-					{@render renderItem(index)}
-				</div>
-			{/if}
+			<div class="flex flex-1" animate:flip={{ duration: 200 }}>
+				{#if item?.isVisible}
+					<div class="item bg-teal-900 flex flex-1">
+						{@render renderItem(index)}
+					</div>
+				{/if}
+			</div>
 		{/each}
 	</div>
-</section>
+</div>
 
 <style>
+	.item {
+		display: flex;
+	}
 	.container-wrapper {
 		overflow: auto;
 		/* overflow-y: hidden; */
